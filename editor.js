63e43131134a336d9c7dc1b7d54ad81b7d38eebf
raizuuuuu <raizutiamat@gmail.com>
2019-10-21 18:02:06 +0800

    no commit

--- editor.js
+++ editor.js
@@ -6,9 +6,7 @@ class SimpleElement {
     children = [];
     text = "";
 }
-
 function start_editor(){
-    /*
     var workingString = window.sessionStorage.getItem("workingData");
     if(!workingString){
         window.workingData = {
@@ -17,23 +15,50 @@ function start_editor(){
     }else{
         window.workingData = JSON.parse(workingString);
     }
-    console.dir(window.workingData);*/
-    add_slide("Slide 1");
+    console.dir(window.workingData);
+    document.getElementById("horizontalTabs1").addEventListener('reorder',
+        (ev) => {console.dir(ev)}
+    );
+    //repeated code -- TODO factorize
+    var template = document.getElementById('new-slide');
+    var clone = document.importNode(template.content, true);
+    select_slide(0).appendChild(clone);
+}
+function select_slide(i){
+    return document.querySelectorAll("smart-tabs smart-tab-item")[i];
 }
 /**
  * 
- * @param {string} label label name
+ * @param {string} t qn or info
+ * @param {[SimpleElement]} c HTML content of slide
+ * @param {number} formid index of form in document. Null if not a question.
+ * @param {[number]} correctAnswer index position for correct answer. More than one value if checkbox. Empty if not a question.
  */
-function add_slide(label){
-    /*
+function add_slide(t, c = [], formid = null, correctAnswer = []){
     var slide = {
         type: t,
         formIndex: formid,
         content: c,
         answer: correctAnswer
-    };*/
-    const tabs = document.querySelector('smart-tabs'); 
-    tabs.insert(2,{"label":label});
-    //window.workingData.slides.push(slide);
+    };
+    window.workingData.slides.push(slide);
     //TODO: render slide. Use DOMParser for content
+}
+/**
+ * 
+ * @param {Element} infoBtn 
+ */
+function add_info_slide(infoBtn) {
+    infoBtn.parentElement.contentEditable = "true";
+    infoBtn.parentElement.innerHTML = "<p>Click here to edit text</p>"
+}
+function detect_new_slide(ev){
+    var template = document.getElementById('new-slide');
+    var clone = document.importNode(template.content, true);
+    if(!select_slide(ev.detail.index).content){
+        console.dir(clone);
+        Array.from(clone.children).forEach((btn) => {
+            select_slide(ev.detail.index).content += btn.outerHTML;
+        });
+    }
 }
