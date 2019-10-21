cb1176373e42df0521b6c61d13fd18b2dca0423a
PixlRainbow <yongzhengleow.17@ichat.sp.edu.sg>
2019-10-21 03:00:47 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+function open_file_dialog(){
+    document.querySelector("#content > input[type=file]").click();
+}
+function load_file(ev){
+    try {
+        if(ev.target.files.length > 0){
+            var jsonfile = ev.target.files[0];
+            (new Response(jsonfile)).json().then((obj) => {
+                console.dir(obj);
+                window.workingData = obj;
+                alert("yay");
+            }).catch((err) => {
+                alert(err);
+            });
+        }
+    } catch (error) {
+        alert(error);
+    }
+}
+// function init(){
+//     try {
+//         document.querySelector("#content > input[type=file]").addEventListener("input", load_file);
+//     } catch (error) {
+//         alert(error);
+//     }
+// }
