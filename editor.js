d3f62ae438f7703e295b3da2bcb7aa484f15096f
PixlRainbow <yongzhengleow.17@ichat.sp.edu.sg>
2019-10-21 06:12:25 +0000

    WIP slide adding function

--- editor.js
+++ editor.js
@@ -1,3 +1,11 @@
+class SimpleElement {
+    tagName = "";
+    id = "";
+    classes = "";
+    /** @type {[SimpleElement]} children*/
+    children = [];
+    text = "";
+}
 function start_editor(){
     var workingString = window.sessionStorage.getItem("workingData");
     if(!workingString){
@@ -8,4 +16,21 @@ function start_editor(){
         window.workingData = JSON.parse(workingString);
     }
     console.dir(window.workingData);
+}
+/**
+ * 
+ * @param {string} t qn or info
+ * @param {[SimpleElement]} c HTML content of slide
+ * @param {number} formid index of form in document. Null if not a question.
+ * @param {[number]} correctAnswer index position for correct answer. More than one value if checkbox. Empty if not a question.
+ */
+function add_slide(t, c = [], formid = null, correctAnswer = []){
+    var slide = {
+        type: t,
+        formIndex: formid,
+        content: c,
+        answer: correctAnswer
+    };
+    window.workingData.slides.push(slide);
+    //TODO: render slide. Use DOMParser for content
 }
