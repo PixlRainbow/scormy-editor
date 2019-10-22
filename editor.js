7d7001d1875b2aa7f7699245b258fbac6df75057
PixlRainbow <yongzhengleow.17@ichat.sp.edu.sg>
2019-10-22 02:53:35 +0000

    save info slides to JSON project file

--- editor.js
+++ editor.js
@@ -34,3 +34,30 @@ function add_slide(t, c = [], formid = null, correctAnswer = []){
     window.workingData.slides.push(slide);
     //TODO: render slide. Use DOMParser for content
 }
+/**
+ * 
+ * @param {Element} infoBtn 
+ */
+function add_info_slide(infoBtn) {
+    infoBtn.parentElement.contentEditable = "true";
+    infoBtn.parentElement.innerHTML = "<p>Click here to edit text</p>"
+}
+function detect_new_slide(ev){
+    var template = document.getElementById('new-slide');
+    var clone = document.importNode(template.content, true);
+    var index = ev.detail.index;
+    if(!select_slide(index).content){
+        console.dir(clone);
+        Array.from(clone.children).forEach((btn) => {
+            select_slide(index).content += btn.outerHTML;
+        });
+    }
+}
+function save_slides(){
+    Array.from(document.querySelectorAll("smart-tabs smart-tab-item")).forEach((elem) => {
+        //TODO: detect if slide is qn slide
+        add_slide("info", elem.firstElementChild.innerHTML);
+    });
+    var blob = new Blob([JSON.stringify(window.workingData)], {type: "application/json;charset=utf-8"});
+    saveAs(blob, "quiz.json");
+}
