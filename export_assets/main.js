1c8c22ab29eddd21e7e72100d5c3e054e249f37d
raizuuuuu <raizutiamat@gmail.com>
2019-10-25 16:36:01 +0800

    too much files

---
+++ export_assets/main.js
@@ -0,0 +1,28 @@
+function open_file_dialog(){
+    document.querySelector("input[type=file]").click();
+}
+function load_file(ev){
+    try {
+        if(ev.target.files.length > 0){
+            var jsonfile = ev.target.files[0];
+            (new Response(jsonfile)).json().then((obj) => {
+                console.dir(obj);
+                //window.workingData = obj;
+                window.sessionStorage.setItem('workingData', JSON.stringify(obj))
+                alert("yay");
+                location.href = "viewer.html";
+            }).catch((err) => {
+                alert(err);
+            });
+        }
+    } catch (error) {
+        alert(error);
+    }
+}
+// function init(){
+//     try {
+//         document.querySelector("#content > input[type=file]").addEventListener("input", load_file);
+//     } catch (error) {
+//         alert(error);
+//     }
+// }
