7099a1bd83ae3430cb0bd0bb5b7c06f92ba4b5f7
PixlRainbow <yongzhengleow.17@ichat.sp.edu.sg>
2019-10-25 07:50:24 +0000

    scorm format generation

---
+++ export_assets/online.js
@@ -0,0 +1,28 @@
+function start_viewer(){
+    var tabs = document.getElementById("horizontalTabs1");
+
+    fetch("quiz.json")
+        .then(function(res){
+            if(res.status != 200){
+                return res.json();
+            }
+            else{
+                var error = new Error(res.statusText);
+                error.response = res;
+                throw error;
+            }
+        })
+        .then(function(workingData){
+            for(let i = 0; i < workingData.length; i++){
+                let thisSlide = workingData.slides[i];
+                console.log(content);
+                tabs.insert(i, {
+                    "label": thisSlide.name,
+                    "content": thisSlide.content
+                });
+            }
+        })
+        .catch(function(err){
+            alert(err);
+        });
+}
